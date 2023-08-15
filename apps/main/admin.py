db69fec925907d30b151730e0305a3c1126c870f
RamazanImashov <ramazanimashov@gmil.com>
2023-08-15 22:53:03 +0600

    second commit

--- apps/main/admin.py
+++ apps/main/admin.py
@@ -3,7 +3,9 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Task
+from .models import Task, Project, Comment
 
 
 admin.site.register(Task)
+admin.site.register(Project)
+admin.site.register(Comment)
